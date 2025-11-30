64879867e20862810e679705c8aa7d7963162bc9
Muthuu SVS <muthuu.svs@outlook.com>
2025-11-29 22:42:41 -0800

    initial function setup for statemachine

--- Src/state_machine.c
+++ Src/state_machine.c
@@ -1,15 +1,37 @@
 /**
  ******************************************************************************
- * @file           : state_machine.c
+ * @file           : state_machine.h
  * @author         : Muthuu SVS
- * @brief          : state_machine switching logic, #defines, etc
+ * @brief          : state_machine switching logic for overall system, #defines, etc
  ******************************************************************************
  */
 
 #include "state_machine.h"
 #include "timers.h"//for timer access
 #include "pwm.h"//for setting uled access and servo control
-#include "adc.h"//sensor reading
+#include "soil_sensor.h"//for soil sensor state machine
+#include "log.h"//debug
+
+/*
+ * @param none 
+ * @return none
+ * Reference : 
+ */
+void water_fsm_init(void)
+{
+    // Initialization code for the watering state machine
+    LOG("Water FSM Initialized\r\n");
+}
+
+/*
+ * @param none 
+ * @return none
+ * Reference : 
+ */
+void water_fsm_run(void)
+{
+    LOG("Water FSM Running\r\n");
+}
 
 
 
--- Src/state_machine.h
+++ Src/state_machine.h
@@ -2,7 +2,7 @@
  ******************************************************************************
  * @file           : state_machine.h
  * @author         : Muthuu SVS
- * @brief          : state_machine switching logic, #defines, etc
+ * @brief          : state_machine switching logic for overall system, #defines, etc
  ******************************************************************************
  */
 
@@ -10,11 +10,37 @@
 #define STATE_MACHINE_H
 #include "log.h"//debug 
 
+//normal sample rate: every 60 seconds
+
+//watering sample rate every 300-500ms its a small plant
+
+
+//states for watering state machine
+typedef enum {
+    WATER_IDLE = 0,
+    WATER_MEASURE,
+    WATER_WAIT_MEASURE,
+    WATER_DECIDE,
+    WATER_OPEN_VALVE,
+    WATER_WATERING,
+    WATER_CLOSE_VALVE
+} water_state_t;
+
+
 /*
  * @param none 
  * @return none
  * Reference : 
  */
+void water_fsm_init(void);
+
+/*
+ * @param none 
+ * @return none
+ * Reference : 
+ */
+void water_fsm_run(void);
+
 
 
 #endif//state_machine_h
